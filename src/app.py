effe329a8886310125c6a2fc09f674cc0a74820a
Nicolas-DL <nicodlopez3@gmail.com>
2024-11-22 16:02:34 +0000

    app_casi

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from pickle import load
 import streamlit as st
+from PIL import Image
 
 model = load(open("/workspaces/proyecto_final_estefanico/models/random_forest.sav", "rb"))
 
@@ -7,7 +8,8 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #dbd9d9; /* Color gris claro */
+        background-color: #f2f2f2; /* Color de fondo */
+        font-family: "Roboto", serif;
     }
     </style>
     """,
@@ -18,61 +20,69 @@ st.markdown(
 st.markdown(
     """
     <style>
-    .stApp h1 {
-        color: #010369; /* Cambia este valor al color que desees */
+    .stApp 
+        h1 {
+        
+        color: #231f20; /* Cambia este valor al color que desees */
+        font-family: "Source Sans Pro", sans-serif;
         font-weight: bold; /* Opcional: pone el título en negrita */
-        text-align: center; /* Opcional: centra el título */
+        text-align: left; /* Opcional: centra el título */
     }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+def main(imagen):
+    
+    img = Image()
+    foto = img.imagen
+
+main('tiplogo')
+
 # Título principal de la aplicación
-st.title('TIP: Perfila al usuario por sus ingresos económicos')
+st.title('Perfila el ingreso')
 
 st.markdown(
     """
     <style>
     /* Cambiar el color y formato de los labels de los radios */
     .stRadio label {
-        color: #010369; /* Color del texto */
+        color: #231f20; /* Color del texto */
         font-size: 30px; /* Tamaño del texto */
         font-weight: bold; /* Negrita */
-        font-family: 'Arial', sans-serif; /* Fuente */
+        font-family: "Roboto", serif; /* Fuente */
         text-align: left; /* Alineación */
     }
 
     /* Cambiar el color y formato de los labels de los sliders */
     .stSlider label {
-        color: #010369; /* Color del texto */
+        color: #231f20; /* Color del texto */
         font-size: 30px; /* Tamaño del texto */
         font-weight: bold; /* Negrita */
-        font-family: 'Helvetica', sans-serif; /* Fuente */
+        font-family: "Roboto", serif; /* Fuente */
         text-align: center; /* Alineación */
     }
 
     /* Cambiar el color de fondo y texto de los labels */
     .stCheckbox label {
+        background-color: #fff;
         color: #008080; /* Color del texto */
         font-size: 30px; /* Tamaño del texto */
-        font-weight: normal; /* Negrita */
+        font-family: "Roboto", serif;
+        font-weight: 400; /* Negrita */
         text-align: center; /* Alineación */
     }
 
-#Variables
-st.subheader("Región: ")
-st.markdown(
-
     /* Cambiar el color de los botones seleccionados */
     .stRadio > div > div > div > label {
-        background-color: #010369; /* Color de fondo de las opciones seleccionadas */
+        background-color: #f2ca4b; /* Color de fondo de las opciones seleccionadas */
         border-radius: 5px;
     }
 
     /* Cambiar el color del texto al pasar el mouse por encima */
     .stRadio > div > div > div > label:hover {
-        background-color: #010369; /* Color cuando el cursor pasa por encima */
+        background-color: #231f20; /* Color cuando el cursor pasa por encima */
         color: white; /* Color del texto cuando se selecciona */
     }
     </style>
@@ -85,6 +95,7 @@ st.markdown(
     <style>
     /* Cambiar el color de fondo y el color del texto en el slider */
     .stSlider div {
+        font-family: "Roboto", serif;
         background-color: ##d9dadb; /* Color de fondo del slider */
         color: #010369; /* Color del texto */
         font-size: 16px; /* Tamaño del texto */
@@ -97,7 +108,7 @@ st.markdown(
     }
 
     .stSlider > div > div > input {
-        background-color: #010369; /* Color de la barra del slider */
+        background-color: #f2ca4b; /* Color de la barra del slider */
         width: 20px; /* Ancho del slider */
     }
     </style>
@@ -105,20 +116,20 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.subheader("Pertenencia a zona urbana o rural")
+st.subheader("¿Pertenece a zona urbana o rural?")
 area =  st.radio(
-    "Seleccione el área al cual pertenece",
+    "Seleccione un área",
     ['Urbano','Rural'],
     index = None)
 
 st.subheader("Nivel socioeconómico")
 nsoceco = st.selectbox(
-    "Seleccione su nivel socioeconómico",
+    "Seleccione un nivel socioeconómico",
     ['Bajo', 'Bajo-medio', 'Bajo-medio-alto', 'Bajo-alto', 'Medio','Medio-alto','Alto'],
     index = None)
 
 st.subheader("¿Cuántas personas viven en el hogar?")
-personas_x_hogar = st.slider('Seleccione un número', min_value=1, max_value=11, step=1)
+personas_x_hogar = st.slider('Seleccione un número', min_value=0, max_value=11, step=1)
 
 st.subheader("Nivel educacional:")
 educ = st.selectbox(
@@ -135,14 +146,14 @@ sprev_salud =  st.radio(
 
 st.subheader("Condición laboral")
 activ = st.radio(
-    "Seleccione condición laboral actual",
+    "Seleccione la condición laboral",
     ['Ocupado(a)', 'Desocupado(a) (busca empleo)', 'Inactivo(a) (no busca empleo)'],
     index = None
 )
 
-st.subheader("Propiedad de la vivienda actual")
+st.subheader("Propiedad de la vivienda")
 ten_viv = st.radio(
-  "Seleccione propiedad de la vivienda",
+  "Seleccione un tipo",
   ['Propia', 'Arrendada', 'Cedida', 'Poseedor/ocupante irregular, usufructo u otro'],
   index = None
 )
@@ -193,28 +204,29 @@ st.markdown(
     """
     <style>
     .stButton>button {
-        background-color: #010369; /* Color de fondo del botón (Azul) */
-        color: white; /* Color del texto */
-        font-size: 30px; /* Tamaño de la fuente */
+        background-color: #f2ca4b; /* Color de fondo del botón (Amarillo) */
+        color: black; /* Color del texto */
+        font-size: 45px; /* Tamaño de la fuente */
         font-weight: bold; /* Negrita */
         border-radius: 10px; /* Bordes redondeados */
-        border: none; /* Sin borde */
-        padding: 15px 25px; /* Espaciado interno */
+        padding: 16px 24px; /* Espaciado interno */
         transition: background-color 0.3s ease; /* Efecto de transición para el color de fondo */
     }
 
     .stButton>button:hover {
-        background-color: #010369; /* Color de fondo cuando se pasa el mouse*/
+        background-color: #f8c111; /* Color de fondo cuando se pasa el mouse*/
+        color: #231f20;
+        border: solid black 1px;
     }
     </style>
     """, 
     unsafe_allow_html=True
 )
 
-if st.button('Haz clic aquí'):
+if st.button('Predecir ingreso'):
     prediccion = model.predict([
         [area_dic[area], nsoceco_dic[nsoceco], personas_x_hogar,  educ_dic[educ],
         sprev_salud_dic[sprev_salud], activ_dic[activ], ten_viv_dic[ten_viv]]
-    ])
+    ]) 
     st.write('Ingreso estimado:', prediccion)
-
+       
